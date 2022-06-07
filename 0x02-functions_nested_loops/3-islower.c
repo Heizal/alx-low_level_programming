7960e4ebd989c93fb586ea2528b0332cab236aaa
Heizal <patriciaheizal@gmail.com>
2022-06-07 04:26:38 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase character
- * @c- the character to check for
+ * @c: the character to check for
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
